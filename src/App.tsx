7faf889327e3458a44d53b7c8154cad4a3213232
Aditya Karnam <akarnam37@gmail.com>
2024-11-26 14:43:30 -0600

    fix: update css for popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
 
 function App() {
-  // start bilding your popup app here
-  return <h1 className="text-3xl text-center font-bold underline">Guardian Sight</h1>;
+  return (
+    <div className="popup-container">
+      <h1 className="text-3xl font-bold">Guardian Sight</h1>
+    </div>
+  );
 }
 
 export default App;
