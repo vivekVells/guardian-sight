3de228be194cd286ea974a45b768e1d93e0b9a43
Aditya Karnam <akarnam37@gmail.com>
2024-11-28 01:54:19 -0600

    fix: deploy script updates

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,10 @@
 echo "Starting deployment"
 echo "Pulling from git"
 git pull origin main
+echo "Installing dependencies"
+npm install
+echo "Building"
+npm run build
+echo "Restarting server"
 npx pm2 restart guardian-sight-api
 echo "Deployed successfully"
