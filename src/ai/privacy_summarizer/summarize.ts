2feb37ec70fca84e48f3168564366e886ffe6ed3
Aditya Karnam <akarnam37@gmail.com>
2024-12-01 00:06:51 -0600

    feat: add final stage to present data by category

--- src/ai/privacy_summarizer/summarize.ts
+++ src/ai/privacy_summarizer/summarize.ts
@@ -1,22 +1,28 @@
 import BulletConsolidator from "../bullet_consolidator/stage";
 import BulletGenerator from "../bullet_generator/stage";
+import SummarizerPresenter from "../summarizer_presenter/bullet_generator/stage";
 
 export const ROLE = "privacy_summarizer";
 
 class PrivacySummarizer {
   private bulletGenerator: BulletGenerator;
   private bulletConsolidator: BulletConsolidator;
+  private summarizerPresenter: SummarizerPresenter;
 
   constructor() {
     this.bulletGenerator = new BulletGenerator();
     this.bulletConsolidator = new BulletConsolidator();
+    this.summarizerPresenter = new SummarizerPresenter();
   }
 
   public async summarizePrivacyPolicy(text: string): Promise<string> {
     const bulletPoints = await this.bulletGenerator.generateBulletPoints(text);
     const bulletPointsConsolidated =
       await this.bulletConsolidator.consolidateBulletPoints(bulletPoints);
-    return bulletPointsConsolidated;
+    const summary = await this.summarizerPresenter.summarizeAndPresent(
+      bulletPointsConsolidated
+    );
+    return summary;
   }
 }
 
